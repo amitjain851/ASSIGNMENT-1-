ef5e212cce304a23e88e4ae957f5a0ffc496891e
amitjain851 <amitjain.851@hotmail.com>
2025-03-09 10:34:41 +0530

    Add files via upload
    Initial commit with Python scripts

---
+++ Mathematical Operations.py
@@ -0,0 +1,11 @@
+#Task 1: Perform Basic Mathematical Operations
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+add = (a + b)
+sub = (a - b)
+multi = (a * b)
+div = (a / b)
+print("Addition:" ,add)
+print("Subtraction: " ,sub)
+print("Multiplication: " ,multi)
+print("Division: " ,div)
